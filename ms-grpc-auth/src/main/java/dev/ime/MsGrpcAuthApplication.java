b6df35b31cf3a4110ce865765398306b3a0896d2
IvanM <87393011+Ivan-Montes@users.noreply.github.com>
2025-04-03 07:37:17 +0200

    feat: initial commit ms-grpc-auth

---
+++ ms-grpc-auth/src/main/java/dev/ime/MsGrpcAuthApplication.java
@@ -0,0 +1,13 @@
+package dev.ime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsGrpcAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsGrpcAuthApplication.class, args);
+	}
+
+}
