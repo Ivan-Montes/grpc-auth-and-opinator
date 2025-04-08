20fb822c7a3dc2afbc51be76468a4ad390890ed3
IvanM <87393011+Ivan-Montes@users.noreply.github.com>
2025-04-08 07:28:25 +0200

    feat: add inherit exceptions

---
+++ ms-grpc-auth/src/main/java/dev/ime/application/exception/GrpcClientCommunicationException.java
@@ -0,0 +1,19 @@
+package dev.ime.application.exception;
+
+import java.util.Map;
+import java.util.UUID;
+
+import dev.ime.common.config.GlobalConstants;
+
+public class GrpcClientCommunicationException extends BasicException{
+
+	private static final long serialVersionUID = -4800491429907807948L;
+
+	public GrpcClientCommunicationException(Map<String, String> errors) {
+		super(
+				UUID.randomUUID(), 
+				GlobalConstants.EX_GRPCCLIENTCOM, 
+				GlobalConstants.EX_GRPCCLIENTCOM_DESC, 
+				errors);
+	}
+}
