4b2f1ba8713c73008960deefc4a280d330afb90f
IvanM <87393011+Ivan-Montes@users.noreply.github.com>
2025-04-21 07:30:58 +0200

    feat: add read repo port

---
+++ ms-grpc-user/src/main/java/dev/ime/domain/port/outbound/ReadRepositoryPort.java
@@ -0,0 +1,17 @@
+package dev.ime.domain.port.outbound;
+
+import java.util.UUID;
+
+import org.springframework.data.domain.Pageable;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+public interface ReadRepositoryPort<T> {
+
+	Flux<T> findAll(Pageable pageable);
+	Mono<T> findById(UUID id);
+	Mono<T> findByEmail(String email);
+	Mono<Long> countByEmail(String email);
+
+}
