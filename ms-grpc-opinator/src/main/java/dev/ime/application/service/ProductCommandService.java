475abd49aed013c4a84631dcf8a34fbfe869fea8
IvanM <87393011+Ivan-Montes@users.noreply.github.com>
2025-06-13 07:36:06 +0200

    feat: add Product command service

---
+++ ms-grpc-opinator/src/main/java/dev/ime/application/service/ProductCommandService.java
@@ -0,0 +1,82 @@
+package dev.ime.application.service;
+
+import java.util.Map;
+import java.util.UUID;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+import dev.ime.application.dto.ProductDto;
+import dev.ime.application.exception.ResourceNotFoundException;
+import dev.ime.application.usecases.command.CreateProductCommand;
+import dev.ime.application.usecases.command.DeleteProductCommand;
+import dev.ime.application.usecases.command.UpdateProductCommand;
+import dev.ime.common.constants.GlobalConstants;
+import dev.ime.domain.command.Command;
+import dev.ime.domain.model.Event;
+import dev.ime.domain.port.inbound.CommandDispatcher;
+import dev.ime.domain.port.inbound.CommandServicePort;
+import dev.ime.domain.port.outbound.PublisherPort;
+import reactor.core.publisher.Mono;
+
+@Service
+public class ProductCommandService implements CommandServicePort<ProductDto> {
+
+	private final CommandDispatcher commandDispatcher;
+	private final PublisherPort publisherPort;
+	private static final Logger logger = LoggerFactory.getLogger(ProductCommandService.class);
+
+	public ProductCommandService(@Qualifier("productCommandDispatcher")CommandDispatcher commandDispatcher, PublisherPort publisherPort) {
+		super();
+		this.commandDispatcher = commandDispatcher;
+		this.publisherPort = publisherPort;
+	}
+
+	@Override
+	public Mono<Event> create(ProductDto dto) {
+
+		return Mono.justOrEmpty(dto)
+				.switchIfEmpty(Mono.error(new ResourceNotFoundException(Map.of(GlobalConstants.PROD_CAT, GlobalConstants.MSG_REQUIRED))))						
+				.map(item -> new CreateProductCommand(UUID.randomUUID(), dto.productName(), dto.productDescription(), dto.categoryId()))
+				.flatMap(this::runHandler)
+				.flatMap(this::processEvents);	
+	}
+
+	@Override
+	public Mono<Event> update(ProductDto dto) {
+
+		return Mono.justOrEmpty(dto)
+				.switchIfEmpty(Mono.error(new ResourceNotFoundException(Map.of(GlobalConstants.PROD_CAT, GlobalConstants.MSG_REQUIRED))))						
+				.map(item -> new UpdateProductCommand(dto.productId(), dto.productName(), dto.productDescription(), dto.categoryId()))
+				.flatMap(this::runHandler)
+				.flatMap(this::processEvents);	
+	}
+
+	@Override
+	public Mono<Event> deleteById(UUID id) {
+
+		return Mono.justOrEmpty(id)
+				.switchIfEmpty(Mono.error(new ResourceNotFoundException(Map.of(GlobalConstants.PROD_CAT, GlobalConstants.MSG_REQUIRED))))						
+				.map(item -> new DeleteProductCommand(id))
+				.flatMap(this::runHandler)
+				.flatMap(this::processEvents);	
+	}
+
+	private Mono<Event> runHandler(Command command){
+		
+		return Mono.just(command)
+				.map(commandDispatcher::getCommandHandler)
+				.flatMap( handler -> handler.handle(command));		
+	}
+	
+	private Mono<Event> processEvents(Event event) {
+		
+	    return Mono.just(event)	
+	        .doOnNext(eventItem -> logger.info(GlobalConstants.MSG_PATTERN_INFO, GlobalConstants.EVENT_CAT, eventItem))
+	    .flatMap(eventItem -> publisherPort.publishEvent(event).thenReturn(event))
+        .thenReturn(event);
+	}
+
+}
