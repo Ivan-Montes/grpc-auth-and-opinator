b3ca5a28bf0ba394236e0b13f3faff566b37ea2a
IvanM <87393011+Ivan-Montes@users.noreply.github.com>
2025-06-17 07:26:57 +0200

    feat: add Kafka Vote subscriber

---
+++ ms-grpc-opinator/src/main/java/dev/ime/infrastructure/adapter/KafkaVoteSubscriberAdapter.java
@@ -0,0 +1,84 @@
+package dev.ime.infrastructure.adapter;
+
+import java.util.Map;
+import java.util.function.Function;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+import dev.ime.application.exception.IllegalHandlerException;
+import dev.ime.application.exception.ValidationException;
+import dev.ime.common.constants.GlobalConstants;
+import dev.ime.domain.model.Event;
+import dev.ime.domain.port.inbound.SubscriberPort;
+import dev.ime.domain.port.outbound.BaseProjectorPort;
+import dev.ime.domain.port.outbound.ExtendedProjectorPort;
+import reactor.core.publisher.Mono;
+
+@Component
+public class KafkaVoteSubscriberAdapter implements SubscriberPort {
+
+	private final BaseProjectorPort baseProjector;
+	private final ExtendedProjectorPort extendedProjector;
+	private final Map<String, Function<Event, Mono<?>>> actionsMap;	
+	private static final Logger logger = LoggerFactory.getLogger(KafkaVoteSubscriberAdapter.class);
+
+	public KafkaVoteSubscriberAdapter(@Qualifier("voteProjectorAdapter")BaseProjectorPort baseProjector, @Qualifier("voteProjectorAdapter")ExtendedProjectorPort extendedProjector
+			) {
+		super();
+		this.baseProjector = baseProjector;
+		this.extendedProjector = extendedProjector;
+		this.actionsMap = initializeActionsMap();
+	}
+
+	private Map<String, Function<Event, Mono<?>>> initializeActionsMap() {
+
+		return Map.of(
+				GlobalConstants.VOT_CREATED, baseProjector::create, 
+				GlobalConstants.VOT_UPDATED, extendedProjector::update, 
+				GlobalConstants.VOT_DELETED, baseProjector::deleteById
+				);
+	}
+
+	@KafkaListener(topics = {GlobalConstants.VOT_CREATED, GlobalConstants.VOT_UPDATED, GlobalConstants.VOT_DELETED}, groupId = "msgrpcopinator-consumer-vote")
+	@Override
+	public Mono<Void> onMessage(ConsumerRecord<String, Event> consumerRecord) {
+
+		return Mono.just(consumerRecord)
+				.flatMap(this::validateTopic)
+				.flatMap(this::validateValue)
+				.doOnNext( c -> logger.info(GlobalConstants.MSG_PATTERN_INFO, c.topic(), c))				
+				.flatMap(this::processEvent)
+				.onErrorResume( error ->  Mono.fromRunnable(()-> logger.error(GlobalConstants.MSG_PATTERN_SEVERE, GlobalConstants.EX_PLAIN, error.getMessage() != null? error.getMessage():GlobalConstants.EX_PLAIN)))
+				.then();		
+	}
+
+	private Mono<ConsumerRecord<String, Event>> validateTopic(ConsumerRecord<String, Event> consumer ){
+		
+		return Mono.justOrEmpty(consumer.topic())
+				.filter( topic -> !topic.trim().isEmpty() )
+				.switchIfEmpty(Mono.error(new ValidationException(Map.of(this.getClass().getSimpleName(), GlobalConstants.EX_VALIDATION_DESC))))
+				.thenReturn(consumer);
+	}
+	
+	private Mono<ConsumerRecord<String, Event>> validateValue(ConsumerRecord<String, Event> consumer ){
+		
+		return Mono.justOrEmpty(consumer.value())
+				.switchIfEmpty(Mono.error(new ValidationException(Map.of(this.getClass().getSimpleName(), GlobalConstants.EX_VALIDATION_DESC))))
+				.thenReturn(consumer);
+	}
+	
+    private Mono<Void> processEvent(ConsumerRecord<String, Event> consumer ){
+    	
+    	return Mono.justOrEmpty(consumer.topic())
+    	.map(actionsMap::get)
+		.switchIfEmpty(Mono.error(new IllegalHandlerException(Map.of(this.getClass().getSimpleName(), consumer.topic()))))
+		.flatMap( function -> function.apply(consumer.value()))
+    	.then();
+    }
+	
+}
