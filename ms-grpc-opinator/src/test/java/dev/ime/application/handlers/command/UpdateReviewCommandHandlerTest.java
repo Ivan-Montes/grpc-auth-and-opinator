d9005e712d2e873dece5897999a086a7cd669b75
IvanM <87393011+Ivan-Montes@users.noreply.github.com>
2025-07-12 07:23:21 +0200

    test: add for update Review handler

---
+++ ms-grpc-opinator/src/test/java/dev/ime/application/handlers/command/UpdateReviewCommandHandlerTest.java
@@ -0,0 +1,116 @@
+package dev.ime.application.handlers.command;
+
+import java.time.Instant;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import dev.ime.application.exception.ResourceNotFoundException;
+import dev.ime.application.usecases.command.UpdateReviewCommand;
+import dev.ime.application.utils.JwtUtil;
+import dev.ime.common.constants.GlobalConstants;
+import dev.ime.common.mapper.EventMapper;
+import dev.ime.domain.command.Command;
+import dev.ime.domain.model.Event;
+import dev.ime.domain.model.Review;
+import dev.ime.domain.port.outbound.EventWriteRepositoryPort;
+import dev.ime.domain.port.outbound.ReadRepositoryPort;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+@ExtendWith(MockitoExtension.class)
+class UpdateReviewCommandHandlerTest {
+
+	@Mock
+	private EventWriteRepositoryPort eventWriteRepository;
+	@Mock
+	private ReadRepositoryPort<Review> reviewRepository;
+	@Mock
+	private JwtUtil jwtUtil;
+	@Mock
+	private EventMapper eventMapper;
+	
+	@InjectMocks
+	private UpdateReviewCommandHandler updateReviewCommandHandler;
+
+	private Event event;
+	private UpdateReviewCommand updateCommand;
+	private Review review;
+
+	private final UUID reviewId = UUID.randomUUID();
+	private final String email = "email@email.tk";
+	private final UUID productId = UUID.randomUUID();
+	private final String reviewText = "Excellent";
+	private final Integer rating = 5;
+
+	private UUID eventId = UUID.randomUUID();
+	private final String eventCategory = GlobalConstants.REV_CAT;
+	private final String eventType = GlobalConstants.REV_DELETED;
+	private final Instant eventTimestamp = Instant.now();
+	private Map<String, Object> eventData = new HashMap<>();
+
+	@BeforeEach
+	private void setUp() {	
+
+		eventData = new HashMap<>();
+		eventData.put(GlobalConstants.REV_ID, reviewId.toString());
+		eventData.put(GlobalConstants.USERAPP_EMAIL, email);
+		eventData.put(GlobalConstants.PROD_ID, productId.toString());
+		eventData.put(GlobalConstants.REV_TXT, reviewText);
+		eventData.put(GlobalConstants.REV_RAT, rating);
+		
+		updateCommand = new UpdateReviewCommand(reviewId, reviewText, rating);		
+
+		event = new Event(
+				eventId,
+				eventCategory,
+				eventType,
+				eventTimestamp,
+				eventData);
+		
+		review = new Review();
+		review.setReviewId(reviewId);
+		review.setEmail(email);
+	}
+
+	@Test
+	void handle_shouldReturnEvent() {
+
+		Mockito.when(reviewRepository.findById(Mockito.any(UUID.class))).thenReturn(Mono.just(review));
+		Mockito.when(jwtUtil.getJwtTokenFromContext()).thenReturn(Mono.just(email));
+		Mockito.when(eventMapper.createEvent(Mockito.anyString(), Mockito.anyString(), Mockito.any(Command.class))).thenReturn(event);		
+		Mockito.when(eventWriteRepository.save(Mockito.any(Event.class))).thenReturn(Mono.just(event));
+
+		StepVerifier
+		.create(updateReviewCommandHandler.handle(updateCommand))
+		.expectNext(event)
+		.verifyComplete();
+
+		Mockito.verify(reviewRepository).findById(Mockito.any(UUID.class));
+		Mockito.verify(jwtUtil).getJwtTokenFromContext();
+		Mockito.verify(eventMapper).createEvent(Mockito.anyString(), Mockito.anyString(), Mockito.any(Command.class));
+		Mockito.verify(eventWriteRepository).save(Mockito.any(Event.class));
+	}
+
+	@Test
+	void handle_UnknownResource_shouldReturnError() {
+		
+		Mockito.when(reviewRepository.findById(Mockito.any(UUID.class))).thenReturn(Mono.empty());
+
+		StepVerifier
+		.create(updateReviewCommandHandler.handle(updateCommand))
+		.expectError(ResourceNotFoundException.class)
+		.verify();
+
+		Mockito.verify(reviewRepository).findById(Mockito.any(UUID.class));
+	}
+
+}
